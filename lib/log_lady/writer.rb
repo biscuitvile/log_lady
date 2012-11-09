4cb0ecb0559161deb21726cc40f2488b1d37f0ea
Andy Borsz <andy.borsz@gmail.com>
2012-11-09 14:40:53 -0500

    Send changes_message as an argument to build_changeset

--- lib/log_lady/writer.rb
+++ lib/log_lady/writer.rb
@@ -3,16 +3,17 @@ module LogLady
     attr_accessor :record
 
     def before_create(record)
-      record.logs.build changeset: build_changeset(record, record.changed_attributes)
+      record.logs.build changeset: build_changeset(record, :changed_attributes)
     end
 
     alias :before_update :before_create
 
     def before_destroy(record)
-      record.logs.create changeset: build_changeset(record, record.predestroy_attributes)
+      record.logs.create changeset: build_changeset(record, :predestroy_attributes)
     end
 
-    def build_changeset(record, changes)
+    def build_changeset(record, changes_message)
+      changes = record.public_send(changes_message)
       changes.inject({}) do | result, (attr, old_val) |
         result[attr] = [ old_val, record.send(attr) ]; result
       end
